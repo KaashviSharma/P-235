67b5de7d02342a931a0e556fa55fd63eeb719d62
KaashviSharma <87973727+KaashviSharma@users.noreply.github.com>
2023-10-11 16:53:06 +0530

    Add files via upload

---
+++ P-235.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt 
+import plotly.graph_objects as go 
+import plotly.express as px
+dataset = pd.read_csv("projectC234_EPL.csv")
+football_club = dataset["Club"].value_counts().head(20)
+print("Top 20 clubs with maximum penalty accuracy premier league \n",football_club)
+club_fig = go.Figure(data = [go.Pie(labels= football_club.index,values = football_club.values,hole = .5)])
+club_fig.show()
